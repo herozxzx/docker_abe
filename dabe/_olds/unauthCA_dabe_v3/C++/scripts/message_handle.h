4b58bd3f7668fbcf9502b8f20c45f276830095ec
herozxzx <43542932+herozxzx@users.noreply.github.com>
2022-11-16 05:44:47 +0900

    fixed bugs

--- dabe/_olds/unauthCA_dabe_v3/C++/scripts/message_handle.h
+++
@@ -1,6 +0,0 @@
-#include <gmp.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-extern void messageToValue(char *message, mpz_t message_mpz, char *m);
-extern void valueToMessage(char *message, mpz_t message_mpz);
